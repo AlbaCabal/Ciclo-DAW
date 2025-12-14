f53ece987f8357de453abd51c0e52bbfe9ed53d1
AlbaCabal <165717154+AlbaCabal@users.noreply.github.com>
2025-12-14 10:51:20 +0100

    Update usuariosmasivos.ps1

--- Sistemas/Windows/usuariosmasivos.ps1
+++ Sistemas/Windows/usuariosmasivos.ps1
@@ -1,8 +1,8 @@
 # Creación de usuarios de forma masiva.
 
-$file_users=Import-Csv -Path usuarios.csv   # Importa el archivo CSV como objetos PowerShell guardandolo en $file_users
+$file_users=Import-Csv -Path usuarios.csv   # Importa el archivo CSV como objetos PowerShell guardándolo en $file_users
 foreach ($user in $file_users) {   # Bucle que recorre cada usuario de $file_users
-  $clave=ConvertTo-SecureString $user.contrasena -AsPlainText -Force   # Convierte la contraseña en un SecureString guardandolo en $clave
+  $clave=ConvertTo-SecureString $user.contrasena -AsPlainText -Force   # Convierte la contraseña en un SecureString guardándolo en $clave
   New-LocalUser $user.cuenta -FullName $user.nombre_completo -Password $clave -Description $user.descripcion   # Crea el usuario local en Windows con nombre, nombre completo, contraseña y descripción
   Add-LocalGroupMember -Group usuarios -Member $user.cuenta   # Agrega el usuario al grupo local “usuarios”
   Write-Host "Usuario $user.cuenta creado"   # Muestra un mensaje en la consola
