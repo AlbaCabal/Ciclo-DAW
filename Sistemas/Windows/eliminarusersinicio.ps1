0753f6f9e141aaef4dcf6d04d72261c01612853b
AlbaCabal <165717154+AlbaCabal@users.noreply.github.com>
2025-12-14 10:40:21 +0100

    Update eliminarusersinicio.ps1

--- Sistemas/Windows/eliminarusersinicio.ps1
+++ Sistemas/Windows/eliminarusersinicio.ps1
@@ -1,7 +1,7 @@
 # Eliminación de usuarios de forma masiva de la lista de autenticación
 
-$fileUsersCsv=Read-Host cuentas.csv
-$file_Users = Get-Content $fileUsersCsv
-foreach($linea in $file_Users) {
-  REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /V $linea "/T" REG_DWORD "/D" 0
+$fileUsersCsv=Read-Host cuentas.csv   # Pide entrada del archivo por consola guardandolo en $fileUsersCsv
+$file_Users = Get-Content $fileUsersCsv   # Lee el contenido de $fileUsersCsv y guarda el contenido en $file_Users
+foreach($linea in $file_Users) {   # Hace un bucle que recorre cada línea (usuario) de $file_Users
+  REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /V $linea "/T" REG_DWORD "/D" 0   # Modifica el Registro de Windows para cada usuario
 }
